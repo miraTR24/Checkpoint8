cd8b40954a0ce5975a1fc162d797c9bfcbb4f946
tiremira <tiremira@gmail.com>
2022-11-12 20:27:41 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import IntroSection from './components/intro/Intro'
+import ContactSection from './components/contact-section/ContactSection'
+import MapSection from './components/map/Map' // import the map here
+import DisclaimerSection from './components/disclaimer/Disclaimer'
+import FooterSection from './components/footer/Footer'
+
+import './App.css'
+
+const location = {
+  address: 'Cité Rabia Tahar Bab Ezzouar',
+  lat: 36.753768,
+  lng: 3.058756,
+} // our location object from earlier
+
+const App = () => (
+  <div className="App">
+    <IntroSection />
+    <ContactSection />
+    <MapSection location={location} zoomLevel={17} /> {/* include it here */}
+    <DisclaimerSection />
+    <FooterSection />
+  </div>
+)
+
+export default App
